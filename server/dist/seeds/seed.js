3f9765cd6d9a39683b2d5ae6c51b1c6948846376
Matt <mwelc64@yahoo.com>
2025-03-16 12:53:02 -0400

    fixed API endpoint for quiz questions

--- server/dist/seeds/seed.js
+++ server/dist/seeds/seed.js
@@ -1,32 +1,11 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import mongoose from 'mongoose';
-import db from '../config/connection.js';
-import { Question } from '../models/index.js';
-
-// Get current directory
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Load JSON with fs
-const pythonQuestionsPath = path.join(__dirname, './pythonQuestions.json');
-const pythonQuestions = JSON.parse(fs.readFileSync(pythonQuestionsPath, 'utf8'));
-
-// Clear the database
+import db from "../config/connection.js";
+import Question from "../models/Question.js";
+import cleanDB from "./cleanDb.js";
+import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
 db.once('open', async () => {
-  try {
-    console.log('Connected to database');
-    // Clear the questions collection
-    await Question.deleteMany({});
-
-    // Insert questions
+    // Remove the parameters since your new cleanDB function doesn't take any
+    await cleanDB();
     await Question.insertMany(pythonQuestions);
-
-    console.log('Database seeded successfully!');
+    console.log('Questions seeded!');
     process.exit(0);
-  } catch (err) {
-    console.error('Error seeding database:', err);
-    process.exit(1);
-  }
 });
